0b30685fe23e6759e7f8934d00560163cb0ed7b6
Enrique Flores Ortiz <floresnenrique1@gmail.com>
2023-06-08 21:11:58 -0600

    Primero Commit

---
+++ Calculadora_Meca/src/Interfaz_Potencia_Electrico.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Interfaz_Potencia_Electrico extends JFrame {
+
+    public Interfaz_Potencia_Electrico(){
+        setTitle("Potencial Electrico");
+        setSize(700, 340);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+}
